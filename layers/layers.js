8e405baffffce7413b3ba22bf78f07ddc1fb45f3
SarahDal <dalrymple.sarah@gmail.com>
2024-11-06 18:08:30 +0000

    Adding new walks, poi, and changing some styling

--- layers/layers.js
+++ layers/layers.js
@@ -1,67 +1,67 @@
 var wms_layers = [];
 
 
-        var lyr_BingAerial_0 = new ol.layer.Tile({
-            'title': 'Bing Aerial',
-            //'type': 'base',
+        var lyr_ThunderforestLandscape_0 = new ol.layer.Tile({
+            'title': 'Thunderforest Landscape',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1'
+            attributions: ' ',
+                url: 'https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=a17c8af5748d4df29e796b6c5dd78826'
             })
         });
-
-        var lyr_OpenTopo_1 = new ol.layer.Tile({
-            'title': 'OpenTopo',
-            //'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'https://tile.opentopomap.org/{z}/{x}/{y}.png '
-            })
-        });
-
-        var lyr_OpenStreetMap_2 = new ol.layer.Tile({
-            'title': 'OpenStreetMap',
-            //'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-        });
-var format_Walks_3 = new ol.format.GeoJSON();
-var features_Walks_3 = format_Walks_3.readFeatures(json_Walks_3, 
+var format_Walks_1 = new ol.format.GeoJSON();
+var features_Walks_1 = format_Walks_1.readFeatures(json_Walks_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Walks_3 = new ol.source.Vector({
+var jsonSource_Walks_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Walks_3.addFeatures(features_Walks_3);
-var lyr_Walks_3 = new ol.layer.Vector({
+jsonSource_Walks_1.addFeatures(features_Walks_1);
+var lyr_Walks_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Walks_3, 
-                style: style_Walks_3,
+                source:jsonSource_Walks_1, 
+                style: style_Walks_1,
                 popuplayertitle: "Walks",
                 interactive: true,
     title: 'Walks<br />\
-    <img src="styles/legend/Walks_3_0.png" /> 8.3 - 10 km<br />\
-    <img src="styles/legend/Walks_3_1.png" /> 10 - 15 km<br />\
-    <img src="styles/legend/Walks_3_2.png" /> 15 - 20 km<br />\
-    <img src="styles/legend/Walks_3_3.png" /> 20 - 25 km<br />\
-    <img src="styles/legend/Walks_3_4.png" /> 25 - 62.1 km<br />'
+    <img src="styles/legend/Walks_1_0.png" /> up to 10 km<br />\
+    <img src="styles/legend/Walks_1_1.png" /> 10 - 15 km<br />\
+    <img src="styles/legend/Walks_1_2.png" /> 15 - 20 km<br />\
+    <img src="styles/legend/Walks_1_3.png" /> 20 - 25 km<br />\
+    <img src="styles/legend/Walks_1_4.png" /> 25 + km<br />'
+        });
+var format_SomeThingsofInterest_2 = new ol.format.GeoJSON();
+var features_SomeThingsofInterest_2 = format_SomeThingsofInterest_2.readFeatures(json_SomeThingsofInterest_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_SomeThingsofInterest_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_SomeThingsofInterest_2.addFeatures(features_SomeThingsofInterest_2);
+var lyr_SomeThingsofInterest_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_SomeThingsofInterest_2, 
+                style: style_SomeThingsofInterest_2,
+                popuplayertitle: "Some Things of Interest",
+                interactive: true,
+    title: 'Some Things of Interest<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_0.png" /> Building<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_1.png" /> Earthworks<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_2.png" /> Memorial<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_3.png" /> Nothing<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_4.png" /> Plane Wreck<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_5.png" /> Ruin<br />\
+    <img src="styles/legend/SomeThingsofInterest_2_6.png" /> Unknown<br />'
         });
 
-lyr_BingAerial_0.setVisible(true);lyr_OpenTopo_1.setVisible(true);lyr_OpenStreetMap_2.setVisible(true);lyr_Walks_3.setVisible(true);
-var layersList = [lyr_BingAerial_0,lyr_OpenTopo_1,lyr_OpenStreetMap_2,lyr_Walks_3];
-lyr_Walks_3.set('fieldAliases', {'fid': 'fid', 'FileName': 'Walk Name', 'Length': 'Length (km)', 'Notes': 'Notes', 'link': 'link', });
-lyr_Walks_3.set('fieldImages', {'fid': 'TextEdit', 'FileName': 'TextEdit', 'Length': 'TextEdit', 'Notes': 'TextEdit', 'link': 'TextEdit', });
-lyr_Walks_3.set('fieldLabels', {'fid': 'hidden field', 'FileName': 'header label - always visible', 'Length': 'inline label - always visible', 'Notes': 'inline label - always visible', 'link': 'no label', });
-lyr_Walks_3.on('precompose', function(evt) {
+lyr_ThunderforestLandscape_0.setVisible(true);lyr_Walks_1.setVisible(true);lyr_SomeThingsofInterest_2.setVisible(true);
+var layersList = [lyr_ThunderforestLandscape_0,lyr_Walks_1,lyr_SomeThingsofInterest_2];
+lyr_Walks_1.set('fieldAliases', {'fid': 'fid', 'FileName': 'Walk Name', 'Length': 'Length (km)', 'Notes': 'Notes', 'link': 'link', });
+lyr_SomeThingsofInterest_2.set('fieldAliases', {'fid': 'fid', 'Name': 'Name', 'Description': 'Description', 'Type': 'Type', });
+lyr_Walks_1.set('fieldImages', {'fid': 'TextEdit', 'FileName': 'TextEdit', 'Length': 'TextEdit', 'Notes': 'TextEdit', 'link': 'TextEdit', });
+lyr_SomeThingsofInterest_2.set('fieldImages', {'fid': 'TextEdit', 'Name': 'TextEdit', 'Description': 'TextEdit', 'Type': 'TextEdit', });
+lyr_Walks_1.set('fieldLabels', {'fid': 'hidden field', 'FileName': 'header label - always visible', 'Length': 'inline label - always visible', 'Notes': 'no label', 'link': 'no label', });
+lyr_SomeThingsofInterest_2.set('fieldLabels', {'fid': 'hidden field', 'Name': 'no label', 'Description': 'no label', 'Type': 'inline label - visible with data', });
+lyr_SomeThingsofInterest_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
